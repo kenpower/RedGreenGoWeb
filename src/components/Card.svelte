4633bc4e92382302019cd7a706fa0b58ca7e393f
Ken Power <github@kenpower.com>
2022-01-29 00:49:33 +0000

    added plain deck of cards

---
+++ src/components/Card.svelte
@@ -0,0 +1,101 @@
+<script>
+    let flipCardTwist = false;
+    export let off = 0;
+    let mv = off + "px";
+</script>
+
+<div class="flip-card"  class:flipCardTwist style="--mv: {mv};">
+    <div class="flip-card-inner">
+        <div class="flip-card-front" on:click={() => (flipCardTwist = true)}>
+            <div class="content">
+                <h1>Testing Inspiration</h1>
+                <p>Stuck?</p>
+                <p>One of these cards might give you an idea to move forward</p>
+            </div>
+        </div>
+        <div class="flip-card-back" on:click={() => (flipCardTwist = false)}>
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .flip-card {
+        position: absolute;
+        top: var(--mv);
+        left: var(--mv);
+        background-color: transparent;
+        --w: 18vw;
+        --h: calc(var(--w) * 1.5);
+        width: var(--w);
+        height: var(--h);
+    }
+
+    .flip-card-inner {
+        position: absolute; 
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: all 0.6s; 
+        transform-style: preserve-3d;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .flipCardTwist .flip-card-inner {
+
+        /* --tx: calc(50vw - (var(--w) / 2)); */
+        --tx: calc(50vw );
+        --ty: calc(50vh - (var(--h) / 2));
+        transform: 
+            translate(var(--w), 0) 
+            rotateY(180deg)
+            scale(calc(2), calc(2))
+            translateZ(+1px); 
+            
+    
+    }
+
+    .flip-card-front,
+    .flip-card-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+    }
+
+    .flip-card-front .content,
+    .flip-card-back .content {
+        padding: 6px;
+    }
+
+    .flip-card-front h1,
+    .flip-card-back h1 {
+        font-size: 1.5em;
+    }
+
+    .flip-card-front p,
+    .flip-card-back p {
+        font-size: 0.8em;
+    }
+
+    .flip-card-front {
+        border-radius: 5px;
+
+        background-color: #bbb;
+        color: black;
+    }
+
+    .flip-card-back {
+        border-radius: 10px;
+        background-color: #2980b9;
+        color: white;
+        transform:   rotateY(180deg);
+    }
+</style>
--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -1,158 +1,184 @@
 <script>
-import HintCard from "./HintCard.svelte";
-import Solutions from "./Solutions.svelte";
-import ModalContainer from "./ModalContainer.svelte";
-import {fade,fly, blur, scale} from "svelte/transition";
-import { tick } from 'svelte';
-import { createEventDispatcher } from 'svelte';
-import Icon from './Icon.svelte';
-import TimedButton from "./TimedButton.svelte";
+    import HintCard from "./HintCard.svelte";
+    import Solutions from "./Solutions.svelte";
+    import ModalContainer from "./ModalContainer.svelte";
+    import Card from "./Card.svelte";
+    import { fade, fly, blur, scale } from "svelte/transition";
+    import { tick } from "svelte";
+    import { createEventDispatcher } from "svelte";
+    import Icon from "./Icon.svelte";
+    import TimedButton from "./TimedButton.svelte";
 
-const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-export let step;
-let isOpenModal;
-let solutionHTML = ""
-let dataReady = false;
-        
-let st;
+    export let step;
+    let isOpenModal;
+    let solutionHTML = "";
+    let dataReady = false;
 
-let showHint = false;
+    let st;
 
-async function showCard() {
-    showHint = true;
-    await tick()
-    showHint = false
-}
+    let showHint = false;
 
-async function showSolution() {
-    isOpenModal = true;
-}
+    async function showCard() {
+        showHint = true;
+        await tick();
+        showHint = false;
+    }
 
-//TODO move into game
-function scrollIntoView() {
-    const el = document.getElementById("theStep");
-    if (!el) return;
-    el.scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+    async function showSolution() {
+        isOpenModal = true;
+    }
 
-$:{
-    console.log(step)
-	if(dataReady) solutionHTML = st.getStepText(step.iteration+1, step.stateName);
-}
+    //TODO move into game
+    function scrollIntoView() {
+        const el = document.getElementById("theStep");
+        if (!el) return;
+        el.scrollIntoView({
+            behavior: "smooth",
+        });
+    }
 
-$: {
-    scrollIntoView(); //no state here, so does it ever get called?
-}
+    $: {
+        console.log(step);
+        if (dataReady)
+            solutionHTML = st.getStepText(step.iteration + 1, step.stateName);
+    }
+
+    $: {
+        scrollIntoView(); //no state here, so does it ever get called?
+    }
 </script>
 
 {#key step}
-<div  out:fade in:fly="{{ delay: 300, y: 400, duration: 1000 }}" class = {step.classes} id = "theStep">
-    <p class = "title">{step.title}</p>
-    <div class ="stepBody">
-        <p>{step.bodyText}</p>
-        <div>
-            <Icon icon="keyboard"/><span> {step.driver} is the driver</span>
-            <span style="display: inline-block;width:2em;"></span>
-            <Icon icon="navigation"/><span> {step.navigator} is the navigator</span>
+    <div
+        out:fade
+        in:fly={{ delay: 300, y: 400, duration: 1000 }}
+        class={step.classes}
+        id="theStep"
+    >
+        <p class="title">{step.title}</p>
+        <div class="stepBody" >
+            <div style="display:flex;">
+                <div class="cardDeck">
+                    <Card off={18} />
+                    <Card off={12} />
+                    <Card off={6} /> 
+                    <Card off={0} />
+                </div>
+                <div>
+                    <p>{step.bodyText}</p>
+                    <div>
+                        <Icon icon="keyboard" /><span>
+                            {step.driver} is the driver</span
+                        >
+                        <span style="display: inline-block;width:2em;" />
+                        <Icon icon="navigation" /><span>
+                            {step.navigator} is the navigator</span
+                        >
+                    </div>
+                </div>
+            </div>
+            <div style="display: flex; justify-content:space-evenly; ">
+                <button on:click={() => dispatch("interact", "done")}
+                    >{step.buttonText}</button
+                >
+                <button on:click={showCard}>Hint</button>
+                <TimedButton on:solution={showSolution} text="Solution" />
+            </div>
         </div>
-        <div style="display: flex; justify-content:space-evenly" >
-            <button on:click="{() => dispatch('interact', 'done')}">{step.buttonText}</button>
-            <button on:click={showCard}>Hint</button>
-            <TimedButton on:solution="{showSolution}" text="Solution"/>
-        </div>
+        <!-- TODO move into game, or should I??? -->
+        <!-- TODO make hint a sub component of modalContainer -->
+        <HintCard {showHint} cardType={step.helpName} />
+        <ModalContainer bind:isOpenModal>
+            <div style="background:white">
+                {@html solutionHTML}
+            </div>
+        </ModalContainer>
     </div>
-    <!-- TODO move into game, or should I??? -->
-    <!-- TODO make hint a sub component of modalContainer -->
-    <HintCard {showHint} cardType = {step.helpName} />
-    <ModalContainer bind:isOpenModal>
-        <div style="background:white">
-            {@html solutionHTML}
-        </div>
-    </ModalContainer>
-</div>
 {/key}
 
 <Solutions bind:this={st} bind:dataReady />
+
 <style>
-span{
-    vertical-align: top;
-    padding-left: 0.3em;
-}
-.step {
-    color: black;
-    width: 100%;
-    border-radius: 5px;
-    border-width: 2px;
-    border-style: solid;
-    margin-top: 10px;
-    min-height: 150px;
-}
+    .cardDeck {
+        position: relative; /*absolute children position relative to this, not body*/
+        min-width: 20vw;
+        min-height: 30vw;
+        margin: 1em;
+    }
+    span {
+        vertical-align: top;
+        padding-left: 0.3em;
+    }
 
-.step p {
-    margin: 2;
-}
+    .step {
+        color: black;
+        width: 100%;
+        border-radius: 5px;
+        border-width: 2px;
+        border-style: solid;
+        margin-top: 10px;
+        min-height: 150px;
+    }
 
-button {
-    position: relative;
-    bottom: 0px;
-}
+    .step p {
+        margin: 2;
+    }
 
-.container .step .stepBody {
-    padding: 5px;
-    display: none;
-}
+    .container .step .stepBody {
+        padding: 5px;
+        display: none;
+    }
 
-.step .title {
-    font-weight: bold;
-    padding: 4px;
-    margin: 0;
-}
+    .step .title {
+        font-weight: bold;
+        padding: 4px;
+        margin: 0;
+    }
 
-.red .title {
-    background-color: var(--testing-red-muted);
-}
+    .red .title {
+        background-color: var(--testing-red-muted);
+    }
 
-.green .title {
-    background-color:var(--coding-green-muted);
-}
+    .green .title {
+        background-color: var(--coding-green-muted);
+    }
 
-.refactor .title {
-    background-color: var(--refactoring-blue-muted);
-}
+    .refactor .title {
+        background-color: var(--refactoring-blue-muted);
+    }
 
-.red.step {
-    background-color: var(--testing-red-hint);
-}
+    .red.step {
+        background-color: var(--testing-red-hint);
+    }
 
-.green.step {
-    background-color:var(--coding-green-hint);
-}
+    .green.step {
+        background-color: var(--coding-green-hint);
+    }
 
-.refactor.step {
-    background-color: var(--refactoring-blue-hint);
-}
+    .refactor.step {
+        background-color: var(--refactoring-blue-hint);
+    }
 
-.red {
-    border-color: var(--testing-red);
-}
+    .red {
+        border-color: var(--testing-red);
+    }
 
-.green {
-    border-color: var(--coding-green);
-}
+    .green {
+        border-color: var(--coding-green);
+    }
 
-.refactor {
-    border-color:var(--refactoring-blue);
-}
+    .refactor {
+        border-color: var(--refactoring-blue);
+    }
 
-.iteration {
-    border-color: black;
-    border-radius: 5px;
-    border-width: 2px;
-    border-style: solid;
-    background-color: white;
-    padding: 8px;
-}
+    .iteration {
+        border-color: black;
+        border-radius: 5px;
+        border-width: 2px;
+        border-style: solid;
+        background-color: white;
+        padding: 8px;
+    }
 </style>
